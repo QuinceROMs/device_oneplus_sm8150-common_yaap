f50b1da004b338ee3d0fded104a337b342673971
Quince <quinceroms@gmail.com>
2024-12-19 23:51:52 +0100

    sm8150-common: Include GApps

--- common.mk
+++ common.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
+# Include GApps
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
